6e00b11833b1dca6532f710faa33bc30a9f5a8f7
Tero <tero@starupcommons.org>
2020-08-27 14:20:44 +0300

    build

---
+++ packages/core/src/index.js
@@ -0,0 +1,7 @@
+export { default as Box } from "./Box";
+export { default as Divider } from "./Divider";
+
+export { default as theme } from "./theme/theme";
+export { default as ThemeProvider } from "./theme/ThemeProvider";
+
+export * from "./theme/theme";
