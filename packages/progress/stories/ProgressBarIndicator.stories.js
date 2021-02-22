127274d13d2ddd01e7d7252097e77ce3aa31d466
Blake <blake_db99@outlook.com>
2021-02-22 10:58:44 -0500

    added stories

--- packages/progress/stories/ProgressBarIndicator.stories.js
+++ packages/progress/stories/ProgressBarIndicator.stories.js
@@ -2,9 +2,9 @@ import React from "react";
 import {ProgressBarIndicator} from "../src/ProgressBarIndicator";
 
 
-export const progressbar1 = () => <ProgressBarIndicator color = "lightgrey" value = {90} max = {100} />;
+export const progressbar1 = () => <ProgressBarIndicator color = "lightgrey"  value = {90} max = {100} />;
 progressbar1.story = {
-  name: "ProgressBar Error", 
+  name: "ProgressBarError", 
 };
 
 export const progressbar2 = () => <ProgressBarIndicator value = {30} max = {100} />;
@@ -15,5 +15,10 @@ progressbar2.story = {
 export const progressbar3 = () => <ProgressBarIndicator value = {100} />;
 progressbar3.story = {
   name: "ProgressBar 100", 
+};
+
+export const progressbar4 = () => <ProgressBarIndicator color = "red" backgroundColor = "lightgrey" value = {70} height = {30} width = "200px" />;
+progressbar4.story = {
+  name: "ProgressBarRandom", 
 };
 
