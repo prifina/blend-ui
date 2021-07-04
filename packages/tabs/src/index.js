32347b62ae04c9ffffb8d9b6c1c7c0435f5ec29a
hamza <hamzahalilovic.12@gmail.com>
2021-07-04 23:55:15 +0200

    merge confilcts

--- packages/tabs/src/index.js
+++ packages/tabs/src/index.js
@@ -1,7 +1,1 @@
-export { default as Tabs } from "./Tabs";
-export { default as Tab } from "./Tabs";
-export { default as TabList } from "./Tabs";
-export { default as TabPanel } from "./Tabs";
-export { default as TabPanelList } from "./Tabs";
-
 export * from "./Tabs";
