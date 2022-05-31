e0a09ba809e215e92c142c88255c41196c57f026
prifina-admin <tero@prifina.com>
2022-05-31 13:21:52 +0300

    blend dependencies

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
+
+STAGE=alpha
 PACKAGE=$1
 
-lerna publish --no-dist-tag  --force-publish @blend-ui/$PACKAGE
+lerna publish --no-dist-tag   --force-publish @blend-ui/$PACKAGE
+
+#lerna publish --no-dist-tag --canary --preid=$STAGE  --force-publish @blend-ui/$PACKAGE
+# lerna publish --canary --preid next
+# uses the next semantic prerelease version with a specific prerelease identifier, e.g.
+# 1.0.0 => 1.0.1-next.0
