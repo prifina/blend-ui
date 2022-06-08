94ed053f0fa3e09a54f6069aaab26a80a50f8998
prifina-admin <tero@prifina.com>
2022-06-08 07:45:56 +0300

    publish script changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,8 +5,8 @@ STAGE=alpha
 PACKAGE=$1
 
 lerna publish --no-dist-tag   --force-publish @blend-ui/$PACKAGE
-
-#lerna publish --no-dist-tag --canary --preid=$STAGE  --force-publish @blend-ui/$PACKAGE
+# lerna publish prepatch --no-dist-tag
+# lerna publish --no-dist-tag --canary --preid=$STAGE  --force-publish @blend-ui/$PACKAGE
 # lerna publish --canary --preid next
 # uses the next semantic prerelease version with a specific prerelease identifier, e.g.
 # 1.0.0 => 1.0.1-next.0
